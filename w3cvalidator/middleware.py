474488c89270dd2eb8e0ceb804aa41d286fb6d55
Andreas Karlsson <andreas.karlsson@area59.se>
2012-09-07 11:52:11 +0200

    more setup

---
+++ w3cvalidator/middleware.py
@@ -0,0 +1,5 @@
+
+class W3CValidatorMiddleware(object):
+
+	def process_response(self, request, response):
+		pass
---
+++ w3cvalidator/tests.py
@@ -0,0 +1,10 @@
+from django.template import Context, Template
+from django.test import TestCase
+
+
+class TemplateTagsTest(TestCase):
+    def test_success_validation(self):
+        pass
+
+    def test_failed_validation(self):
+        pass
